1e4feb37822a6165eefea35631b631984f3ede49
dumbledore0127 <yamamotoyu0127@gmail.com>
2023-03-12 19:09:29 +0900

    refactor: 💡 react component

--- src/navigators/HomeScreen.tsx
+++ src/navigators/HomeScreen.tsx
@@ -1,14 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
-export const HomeScreen = () => {
-  return (
-    <View style={styles.container}>
-      <Text>Home Screen</Text>
-      <StatusBar style='auto' />
-    </View>
-  );
-};
+export const HomeScreen = () => (
+  <View style={styles.container}>
+    <Text>Home Screen</Text>
+    <StatusBar style='auto' />
+  </View>
+);
 
 const styles = StyleSheet.create({
   container: {
--- src/navigators/index.tsx
+++ src/navigators/index.tsx
@@ -3,10 +3,8 @@ import { HomeScreen } from './HomeScreen';
 
 const Stack = createNativeStackNavigator();
 
-export const AppNavigators = () => {
-  return (
-    <Stack.Navigator>
-      <Stack.Screen name='Home' component={HomeScreen} />
-    </Stack.Navigator>
-  );
-};
+export const AppNavigators = () => (
+  <Stack.Navigator>
+    <Stack.Screen name='Home' component={HomeScreen} />
+  </Stack.Navigator>
+);
